db743743e4754d512f055c2c6b4968d638311945
tehspyke <mr69@students.uwf.edu>
2017-04-08 17:22:04 -0500

    Added some panels

---
+++ HealthyStart/src/gui/LinksPanel.java
@@ -0,0 +1,36 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class LinksPanel extends CustomPanel{
+	
+	private final Action action = new SwingAction();
+	private JFrame frame;
+	
+	public LinksPanel(JFrame frame){
+		super(frame);
+		this.frame = frame;
+		JButton btnNewButton = new JButton("Return to Main Menu");
+		btnNewButton.setAction(action);
+		btnNewButton.setBounds(33, 214, 213, 29);
+		this.add(btnNewButton);
+	}
+	
+	// ActionEvent for Returning to Main Menu
+	@SuppressWarnings("serial")
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "Return to Main Menu");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+			changePanel(new MainMenuPanel(frame));
+		}
+	}
+}
