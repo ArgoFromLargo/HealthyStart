7d615fd81a6a240d92bda3baf0ecc37b0425938a
ArgoFromLargo <lukekledzik@gmail.com>
2017-04-08 03:17:31 -0500

    First push

---
+++ HealthyStart/src/HealthyStartWindow.java
@@ -0,0 +1,54 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+
+public class HealthyStartWindow {
+
+	private JFrame frmHealthyStart;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HealthyStartWindow window = new HealthyStartWindow();
+					window.frmHealthyStart.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public HealthyStartWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmHealthyStart = new JFrame();
+		frmHealthyStart.setBackground(new Color(238, 238, 238));
+		frmHealthyStart.setTitle("Welcome to Healthy Start");
+		frmHealthyStart.setBounds(100, 100, 618, 328);
+		frmHealthyStart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel HealthyStartLogo = new JLabel("");
+		HealthyStartLogo.setHorizontalAlignment(SwingConstants.CENTER);
+		HealthyStartLogo.setIcon(new ImageIcon("/Users/lkledzik/Documents/workspace/HealthyStart/src/HealthyStartLogo.png"));
+		frmHealthyStart.getContentPane().add(HealthyStartLogo, BorderLayout.CENTER);
+	}
+
+}
